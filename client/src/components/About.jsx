5304e6fe53c3c22e59ae435a37d172db46794859
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-12-08 13:28:16 +0530

    about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return <section>
+    /*Container */
+    <div>
+      /* info-left side */
+      <div>
+        <Title 
+        title1={"Your Trusted Real Estate Partner"}
+        title2={"Helping You Every Step of the Way"}
+        para={"Trust, clarity, and simplicity are at the core of everything we do to make your property journey easy."}
+        titleStyles={'mb-10'}/>
+        </div>
+    </div>
+    </section>
 };
 
 export default About;
