37539e3a38e24cf9f4360dad9a0e29b95931a567
Sanduni Vihara Walage <sandunivihara228@gmail.com>
2026-01-28 21:05:14 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,19 +1,28 @@
-import express from "express"
-import cors from 'cors'
-import "dotenv/config"
-import connectDB from "./config/mongodb.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
+await connectDB();
 
-await connectDB()
+const app = express();
+app.use(cors());
 
-const app = express()
-app.use(cors())
+// Middleware Setup
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/clerk", clerkWebhooks);
 
 //Route Endpoint to check API status
-app.get('/', (req,res)=>{
-    res.send("API sucessfully connected")
-})
+app.get("/", (req, res) => {
+  res.send("API sucessfully connected");
+});
 
-const port = process.env.PORT || 4000 
+const port = process.env.PORT || 4000;
 
-app.listen(port, ()=>console.log(`Server is running at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`),
+);
