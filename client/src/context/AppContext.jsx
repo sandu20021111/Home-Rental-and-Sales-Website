f15f800d0039b8cedbfb197eccb16413922d6de4
Sanduni Vihara Walage <sandunivihara228@gmail.com>
2026-02-01 16:11:17 +0530

    update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProperties } from "../assets/data";
 import { Currency } from "lucide-react";
-import { useUser } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
